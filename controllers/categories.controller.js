dbdc0f037e2392e00630503605234612beb23a4a
Shambho jaybhaye <sjpubgplay@gmail.com>
2025-07-22 23:15:16 +0530

    delete category API done

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -33,14 +33,49 @@ async function addCategory(req, res) {
 async function getAllCategories(req, res) {
     try {
         const categories = await Category.find();
-        return res.status(200).json({success:true, count:categories.length, categories});
+        return res.status(200).json({ success: true, count: categories.length, categories });
 
     } catch (error) {
-        console.error('Add Category Error:', error);
+        console.error('get Category Error:', error);
         return res.status(500).json({ success: false, error: 'Internal Server Error' });
     }
 }
+
+// async function updateCategory(req, res) {
+//     try {
+//         const { name, description } = req.body;
+
+//     } catch (error) {
+//         console.error('Update Category Error:', error);
+//         return res.status(500).json({ success: false, error: 'Internal Server Error' });
+//     }
+// }
+
+async function deleteCategory(req, res) {
+    try {
+        const { id } = req.params;
+
+        if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+            return res.status(400).json({ success: false, message: 'Invalid Category ID format' });
+        }
+
+        const category = await Category.findById(id);
+        if (!category) {
+            return res.status(404).json({ success: false, message: 'Category not found' });
+        }
+
+        await Category.deleteOne({ _id: id }); 
+
+        return res.status(200).json({ success: true, message: 'Category deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting category:', error);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+}
+
 export {
     addCategory,
     getAllCategories,
+    // updateCategory,
+    deleteCategory,
 }
--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { addCategory, getAllCategories } from '../controllers/categories.controller.js';
+import { addCategory, getAllCategories, deleteCategory } from '../controllers/categories.controller.js';
 import validateAdmin from '../middlewares/admin.middleware.js';
 const categoryRouter = express.Router();
 
 categoryRouter.post('/', validateAdmin, addCategory);
 categoryRouter.get('/', getAllCategories);
-// categoryRouter.put('/:id');
-// categoryRouter.delete('/:id');
+// categoryRouter.put('/:id', validateAdmin, updateCategory);
+categoryRouter.delete('/:id', validateAdmin, deleteCategory);
 
 export default categoryRouter;
